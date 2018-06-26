82e62582636d5abef156fa791986a0eba753fcb8
BriAnn Nicole Gibson <gibsonbriann@gmail.com>
2018-06-26 17:48:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+  "My name is BriAnn"
+end
+
+get '/hometown' do
+  "My hometown is Memphis TN"
+end
+
+get '/favorite-song' do
+    "My favorite song is Mine by Bizzai"
+  end
 end
